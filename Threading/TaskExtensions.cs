b607207d07c9dc321183b3be66280933e60b85a7
Rezgar <dev@rezgar.pro>
2018-01-22 02:03:14 +0200

    Task extensions

---
+++ Threading/TaskExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezgar.Utils.Threading
+{
+    public static class TaskExtensions
+    {
+        public static bool IsCompletedSuccessfully(this Task task)
+        {
+            return task.Status == TaskStatus.RanToCompletion;
+        }
+    }
+}
